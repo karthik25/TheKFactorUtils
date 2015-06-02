7f4b9cbc73aaa9befabe0d00e864776525e2cbfa
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-01 22:57:12 -0400

    file/folder extensions

---
+++ TheKFactorUtils/Io/FileOrFolderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace TheKFactorUtils.Io
+{
+    public static class FileOrFolderExtensions
+    {
+        public static bool IsDirectory(this string fileOrFolder)
+        {
+            return File.GetAttributes(fileOrFolder) == FileAttributes.Directory;
+        }
+
+        public static bool IsFile(this string fileOrFolder)
+        {
+            return !IsDirectory(fileOrFolder);
+        }
+
+        public static string GetFileName(this string filePath)
+        {
+            return Path.GetFileName(filePath);
+        }
+
+        public static string GetFileExtension(this string filePath)
+        {
+            return Path.GetExtension(filePath);
+        }
+
+        public static string GetDirPath(this string filePath)
+        {
+            return Path.GetDirectoryName(filePath);
+        }
+
+        public static string GetCurrentDir(this string dirPath)
+        {
+            return dirPath.Split('\\').Last();
+        }
+    }
+}
