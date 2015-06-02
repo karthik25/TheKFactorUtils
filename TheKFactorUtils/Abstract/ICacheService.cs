6a6d01ac63a470717972be740650191180d1986b
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-01 22:51:55 -0400

    caching support

---
+++ TheKFactorUtils/Abstract/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TheKFactorUtils.Abstract
+{
+    public interface ICacheService
+    {
+        T Get<T>(string cacheId, Func<T> getItemCallback, int duration) where T : class;
+    }
+}
---
+++ TheKFactorUtils/Caching/CacheService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+using TheKFactorUtils.Abstract;
+
+namespace TheKFactorUtils.Caching
+{
+    public class CacheService : ICacheService
+    {
+        public T Get<T>(string cacheId, Func<T> getItemCallback, int duration = 5) where T : class
+        {
+            var item = HttpRuntime.Cache.Get(cacheId) as T;
+            if (item == null)
+            {
+                item = getItemCallback();
+                HttpContext.Current.Cache.Insert(cacheId,
+                                                 item,
+                                                 null,
+                                                 DateTime.Now.AddMinutes(duration),
+                                                 Cache.NoSlidingExpiration);
+            }
+            return item;
+        }
+    }
+}
