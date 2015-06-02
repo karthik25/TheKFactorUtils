24b2688090919468b433bc7ffecd552356654068
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-01 22:42:27 -0400

    some utils for mvc 5

---
+++ TheKFactorUtils/Mvc/CustomBinders/BindingContextValueExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace TheKFactorUtils.Mvc.CustomBinders
+{
+    public static class BindingContextValueExtensions
+    {
+        public static IEnumerable<string> GetPostedFormKeys(this ControllerContext controllerContext)
+        {
+            var formKeys = controllerContext.HttpContext
+                                            .Request
+                                            .Form
+                                            .AllKeys.ToList();
+            return formKeys;
+        }
+
+        public static string GetValue(this ModelBindingContext bindingContext, string keyName)
+        {
+            if (bindingContext == null || bindingContext.ValueProvider == null)
+                throw new NullReferenceException("Binding context is null");
+
+            var result = bindingContext.ValueProvider.GetValue(keyName);
+            if (result != null)
+            {
+                var values = (string[]) result.RawValue;
+                return values.First();
+            }
+            return null;
+        }
+    }
+}
