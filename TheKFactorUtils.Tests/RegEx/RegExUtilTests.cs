0a76460910ef41155414c81b5deae2fe56b1c9c6
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-04 23:22:00 -0400

    regex utils - inital commit

---
+++ TheKFactorUtils.Tests/RegEx/RegExUtilTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheKFactorUtils.RegexUtils;
+
+namespace TheKFactorUtils.Tests.RegEx
+{
+    [TestClass]
+    public class RegExUtilTests
+    {
+        [TestMethod]
+        public void CanGetAnEnumerableForMatchedGroups()
+        {
+            var regex = new Regex(@"(\d+)\-(\d+)\-(\d+)");
+            const string src = "987-654-3210";
+            var matchGroups = src.GetMatchValues(regex);
+            var expectedValues = new List<string> { "987", "654", "3210" };
+            var index = 0;
+            foreach (var matchGroup in matchGroups)
+            {
+                Assert.AreEqual(expectedValues[index], matchGroup);
+                index++;
+            }
+        }
+    }
+}
---
+++ TheKFactorUtils/RegexUtils/RegExUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TheKFactorUtils.RegexUtils
+{
+    public static class RegExUtil
+    {
+         public static IEnumerable<string> GetMatchValues(this string src, Regex regex)
+         {
+             var match = regex.Match(src);
+             if (!match.Success)
+                 yield break;
+
+             for (var i = 1; i < match.Groups.Count; i++)
+             {
+                 yield return match.Groups[i].Value;
+             }
+         }
+    }
+}
