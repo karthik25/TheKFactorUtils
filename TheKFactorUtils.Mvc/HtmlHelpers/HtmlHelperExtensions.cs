9f24e3aa82c322168d8c52ec2fa425eab263616b
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-07 00:21:00 -0400

    some html helper extns.

---
+++ TheKFactorUtils.Mvc/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace TheKFactorUtils.Mvc.HtmlHelpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string GetRequiredString(this HtmlHelper htmlHelper, string requiredString)
+        {
+            return htmlHelper.ViewContext
+                             .RouteData
+                             .GetRequiredString(requiredString);
+        }
+
+        public static bool IsAuthenticated(this HtmlHelper htmlHelper)
+        {
+            return htmlHelper.ViewContext
+                             .RequestContext
+                             .HttpContext
+                             .Request
+                             .IsAuthenticated;
+        }
+
+        public static UrlHelper GetUrlHelper(this HtmlHelper htmlHelper)
+        {
+            return new UrlHelper(htmlHelper.ViewContext
+                                           .RequestContext);
+        }
+    }
+}
