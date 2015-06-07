646600ac8bf4e6a9cbbc355f182c614e931ea622
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-06 23:57:15 -0400

    more io utils

---
+++ TheKFactorUtils/Abstract/IPathMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace TheKFactorUtils.Abstract
+{
+    public interface IPathMapper
+    {
+        string MapPath(string relativePath);
+    }
+}
---
+++ TheKFactorUtils/Io/PathMapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using TheKFactorUtils.Abstract;
+
+namespace TheKFactorUtils.Io
+{
+    public class PathMapper : IPathMapper
+    {
+        public string MapPath(string relativePath)
+        {
+            return HttpContext.Current
+                              .Server
+                              .MapPath(relativePath);
+        }
+    }
+}
