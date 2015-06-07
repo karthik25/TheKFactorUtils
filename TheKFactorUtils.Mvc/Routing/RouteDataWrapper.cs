5cefc651e8f6f33f3d8ccca2f064d7f5c42e5ee2
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-06-07 00:48:26 -0400

    added a basic route data wrapper

---
+++ TheKFactorUtils.Mvc/Routing/RouteDataWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Routing;
+
+namespace TheKFactorUtils.Mvc.Routing
+{
+    public class RouteDataWrapper
+    {
+        private readonly RouteData _routeData;
+
+        public RouteDataWrapper(RouteData routeData)
+        {
+            _routeData = routeData;
+        }
+
+        public string Controller
+        {
+            get { return _routeData.Values["controller"].ToString(); }
+        }
+
+        public string Action
+        {
+            get { return _routeData.Values["action"].ToString(); }
+        }
+    }
+}
